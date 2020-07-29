3691108150394d6807866f326ded3c874cf53fa3
takamunestudio <takamunetsuboi@gmail.com>
2020-07-29 19:47:39 +0000

    kadai-tsuboi

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+  
+  include SessionsHelper
+  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = current_user.tasks
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が登録されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status, :user)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+end
+
