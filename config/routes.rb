ac3873f963586b7edf63e04863bd4d8103442f33
takamunestudio <takamunetsuboi@gmail.com>
2020-07-30 11:24:06 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+ 
+ root to: 'tasks#index'
+ resources :tasks
   
  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  
-  root to: 'tasks#index'
-  resources :tasks
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
 
-  get 'signup', to: 'users#new'
-  resources :users
+ get 'signup', to: 'users#new'
+ resources :users, only: [:new, :create]
+ 
 end
